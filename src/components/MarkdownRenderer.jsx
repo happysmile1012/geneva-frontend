fb51e77788cdad64904a4813c15b95393d769eed
happysmile1012 <maru.lancers57@gmail.com>
2025-07-18 16:44:38 -0600

    changed filter image in dropdown

--- src/components/MarkdownRenderer.jsx
+++ src/components/MarkdownRenderer.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { Copy, Check } from "lucide-react"; // Icons for copy 
+import { Copy, Check } from "lucide-react"; // Icons for copy
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 
@@ -20,13 +20,12 @@ const CodeBlock = ({ language, children }) => {
 
   useEffect(() => {
     setIsClient(true);
-  }, [])
+  }, []);
 
-  return (
-    isClient && children.indexOf("serpapi.com/searches") > -1 ? 
-    <div 
-      dangerouslySetInnerHTML={{__html: children}}
-    ></div> : <div className="relative bg-[#282C34] text-white rounded-lg overflow-hidden my-4">
+  return isClient && children.indexOf("serpapi.com/searches") > -1 ? (
+    <div dangerouslySetInnerHTML={{ __html: children }}></div>
+  ) : (
+    <div className="relative bg-[#282C34] text-white rounded-lg overflow-hidden my-4">
       {/* Header with Copy Button */}
       <div className="flex justify-between items-center bg-gray-800 px-4 py-2 text-sm font-mono">
         <span className="text-gray-300">{language || "plaintext"}</span>
@@ -56,62 +55,175 @@ const MarkdownRenderer = ({ content, color, bg }) => {
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw]} // Add this to parse HTML
       components={{
-        h1: ({ children }) => <h1 className="text-[45px] font-bold mt-4 mb-2"
-            style={{color: color == "light" || bg ? "black" : "white"}}
-        >{children}</h1>,
-        h2: ({ children }) => <h2 className="text-4xl font-semibold mt-4 mb-2"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</h2>,
-        h3: ({ children }) => <h3 className="text-3xl font-medium mt-4 mb-2"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</h3>,
-        h4: ({ children }) => <h4 className="text-3xl font-medium mt-3 mb-2"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</h4>,
-        h5: ({ children }) => <h5 className="text-2xl font-medium mt-3 mb-2"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</h5>,
-        h6: ({ children }) => <h6 className="text-2xl font-medium mt-3 mb-2"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</h6>,
-        ul: ({ children }) => <ul className="list-disc mt-3 mb-2 pl-6"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</ul>,
-        ol: ({ children }) => <ol className="list-decimal mt-3 mb-2 pl-6"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</ol>,
-        li: ({ children }) => <li className="ml-4 text-2xl"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</li>,
-        p: ({ children }) => <p className="mt-2 mb-2 text-2xl"
-                                style={{color: color == "light" || bg ? "black" : "white"}}>{children}</p>,
-        hr: () => <hr className="mt-5 mb-5 !border-[#ebebeb]"/>,
+        h1: ({ children }) => (
+          <h1
+            className="text-[45px] font-bold mt-4 mb-2"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </h1>
+        ),
+        h2: ({ children }) => (
+          <h2
+            className="text-4xl font-semibold mt-4 mb-2"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </h2>
+        ),
+        h3: ({ children }) => (
+          <h3
+            className="text-3xl font-medium mt-4 mb-2"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </h3>
+        ),
+        h4: ({ children }) => (
+          <h4
+            className="text-3xl font-medium mt-3 mb-2"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </h4>
+        ),
+        h5: ({ children }) => (
+          <h5
+            className="text-2xl font-medium mt-3 mb-2"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </h5>
+        ),
+        h6: ({ children }) => (
+          <h6
+            className="text-2xl font-medium mt-3 mb-2"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </h6>
+        ),
+        ul: ({ children }) => (
+          <ul
+            className="list-disc mt-3 mb-2 pl-6"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </ul>
+        ),
+        ol: ({ children }) => (
+          <ol
+            className="list-decimal mt-3 mb-2 pl-6"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </ol>
+        ),
+        li: ({ children }) => (
+          <li
+            className="ml-4 text-2xl"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </li>
+        ),
+        p: ({ children }) => (
+          <p
+            className="mt-2 mb-2 text-2xl"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </p>
+        ),
+        hr: () => <hr className="mt-5 mb-5 !border-[#ebebeb]" />,
         table: ({ children }) => (
           <div className="overflow-x-auto my-4">
-            <table className="w-full table-auto border border-collapse border-gray-400 text-left text-xl"
-                   style={{color: color == "light" || bg ? "black" : "white"}}>
+            <table
+              className="w-full table-auto border border-collapse border-gray-400 text-left text-xl"
+              style={{ color: color == "light" || bg ? "black" : "white" }}
+            >
               {children}
             </table>
           </div>
         ),
-        thead: ({ children }) => <thead className="bg-gray-200"
-                                        style={{color: color == "light" || bg ? "black" : "white"}}>{children}</thead>,
-        tbody: ({ children }) => <tbody
-            style={{color: color == "light" || bg ? "black" : "white"}}>{children}</tbody>,
-        tr: ({ children }) => <tr className="border-b border-gray-300"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</tr>,
-        th: ({ children }) => <th className="px-4 py-2 font-bold border border-gray-300"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</th>,
-        td: ({ children }) => <td className="px-4 py-2 border border-gray-300"
-                                  style={{color: color == "light" || bg ? "black" : "white"}}>{children}</td>,
+        thead: ({ children }) => (
+          <thead
+            className="bg-gray-200"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </thead>
+        ),
+        tbody: ({ children }) => (
+          <tbody style={{ color: color == "light" || bg ? "black" : "white" }}>
+            {children}
+          </tbody>
+        ),
+        tr: ({ children }) => (
+          <tr
+            className="border-b border-gray-300"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </tr>
+        ),
+        th: ({ children }) => (
+          <th
+            className="px-4 py-2 font-bold border border-gray-300"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </th>
+        ),
+        td: ({ children }) => (
+          <td
+            className="px-4 py-2 border border-gray-300"
+            style={{ color: color == "light" || bg ? "black" : "white" }}
+          >
+            {children}
+          </td>
+        ),
+        img: ({ node, ...props }) => {
+          if (props.src.includes("serpapi.com")) {
+            return (
+              <div className="serpapi-image">
+                <iframe src={props.src} className="w-full h-64" />
+              </div>
+            );
+          }
+          return <img {...props} className="regular-image" />;
+        },
         code({ className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
 
           if (match) {
             // Code block with language
-            return <CodeBlock language={match[1]}
-                              style={{color: color == "light" || bg ? "black" : "white"}}>{children}</CodeBlock>;
+            return (
+              <CodeBlock
+                language={match[1]}
+                style={{ color: color == "light" || bg ? "black" : "white" }}
+              >
+                {children}
+              </CodeBlock>
+            );
           } else if (className === undefined) {
             // Inline code or a code block without language
             if (String(children).includes("\n")) {
-              return <CodeBlock language="plaintext"
-                                style={{color: color == "light" || bg ? "black" : "white"}}>{children}</CodeBlock>;
+              return (
+                <CodeBlock
+                  language="plaintext"
+                  style={{ color: color == "light" || bg ? "black" : "white" }}
+                >
+                  {children}
+                </CodeBlock>
+              );
             } else {
               return (
-                <code className="bg-neutral-700 px-1 py-0.5 rounded" {...props}
-                      style={{color: color == "light" || bg ? "black" : "white"}}>
+                <code
+                  className="bg-neutral-700 px-1 py-0.5 rounded"
+                  {...props}
+                  style={{ color: color == "light" || bg ? "black" : "white" }}
+                >
                   {children}
                 </code>
               );
